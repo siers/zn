12fb6f82dafa1240317b2d5bcbb53cd67d59e808
siers <wimuan@gmail.com>
2016-03-25 18:19:53 +0200

    Use and parse zn.rc configuration file.

--- Bot.hs
+++ Bot.hs
@@ -2,12 +2,23 @@
 
 module Bot where
 
+import Data.Ini
 import Data.Text
 import Network.IRC.Client.Types
 import Network.IRC.Conduit
+import Safe
 
 type BotState = Bool
 type Bot a = StatefulIRC BotState a
+
+data Config = Config
+    { user :: Text
+    , pass :: Text
+    , chans :: [Text]
+
+    , irchost :: Text
+    , ircport :: Int
+    }
 
 target :: Source Text -> Text
 target (Channel chan user) = chan
@@ -19,3 +30,17 @@ from (User user) = user
 
 privtext :: Message Text -> Text
 privtext (Privmsg _from msg) = either (const "") id msg
+
+findValues :: (Text -> Either String Text) -> Either String Config
+findValues lookup = do
+    user <- lookup "user"
+    pass <- lookup "pass"
+    chans <- split (== ',') <$> lookup "chans"
+    irchost <- lookup "irchost"
+    ircport <- maybe (error "cannot parse ircport") id . readMay . unpack
+        <$> lookup "ircport"
+
+    return $ Config user pass chans irchost ircport
+
+parseConfig :: FilePath -> IO (Either String Config)
+parseConfig path = (>>= findValues . flip (lookupValue "main")) <$> readIniFile path
--- Main.hs
+++ Main.hs
@@ -4,7 +4,7 @@ import Bot
 import Commands
 import Control.Concurrent
 import Control.Monad.Reader
-import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS
 import Data.Maybe
 import Data.Text as T hiding (head)
 import GHC.Conc
@@ -17,6 +17,7 @@ import Safe
 import System.Environment
 import Data.Time
 import System.Exit
+import System.Posix.Files
 
 sleep n = liftIO . threadDelay $ n * 1000000
 
@@ -26,36 +27,37 @@ getTVar accessor = accessor >>= liftIO . atomically . readTVar
 setTVar :: MonadIO m => m (TVar b) -> b -> m ()
 setTVar accessor val = accessor >>= liftIO . atomically . flip writeTVar val
 
-initHandler :: Text -> StatefulIRC BotState ()
-initHandler pwd = do
+initHandler :: Text -> [Text] -> StatefulIRC BotState ()
+initHandler pwd chans = do
     liftIO getCurrentTime >>= send . Privmsg "ij" . Right . pack . show
     send . Privmsg "nickserv" . Right $ "id " `append` pwd
-    mapM_ (send . Join) ["#developerslv", "#dirsas"]
+    mapM_ (send . Join) ["#dirsas"]
 
-initDispatcher :: Text -> UnicodeEvent -> StatefulIRC BotState ()
-initDispatcher pwd event = do
+initDispatcher :: Text -> [Text] -> UnicodeEvent -> StatefulIRC BotState ()
+initDispatcher pwd chans event = do
     initialized <- getTVar stateTVar
     when (not initialized) $ do
-        initHandler pwd
+        initHandler pwd chans
     setTVar stateTVar True
 
-conf nick pwd = cfg { _eventHandlers = handlers ++ _eventHandlers cfg }
+conf user pwd chans = cfg { _eventHandlers = handlers ++ _eventHandlers cfg }
     where
-        cfg = defaultIRCConf nick
+        cfg = defaultIRCConf user
         handlers =
-            [ EventHandler "init handler" EMode $ initDispatcher pwd
+            [ EventHandler "init handler" EMode $ initDispatcher pwd chans
             , EventHandler "cmd handler" EPrivmsg cmdHandler]
 
-run :: BS.ByteString -> Int -> Text -> Text -> IO ()
-run host port nick pwd = do
-    conn <- connect' stdoutLogger host port 1 :: IO (ConnectionConfig BotState)
-    startStateful conn (conf nick pwd) False
-
 main = do
-    mayPwd <- fmap pack . headMay <$> getArgs
-
-    when (isNothing mayPwd || fromJust mayPwd == empty) $ do
-        putStrLn $ "Add nickserv password as $1."
+    configFound <- fileExist "zn.rc"
+    when (not configFound) $ do
+        putStrLn "# no conf found\n$ cp zn.rc{sample,}"
         exitFailure
 
-    run "irc.freenode.net" 6667 "pipele" $ fromJust mayPwd
+    config <- either error return =<< parseConfig "zn.rc"
+
+    conn <- connect'
+        stdoutLogger
+        (BS.pack . unpack $ irchost config)
+        (ircport config)
+        1 :: IO (ConnectionConfig BotState)
+    startStateful conn (conf (user config) (pass config) (chans config)) False
