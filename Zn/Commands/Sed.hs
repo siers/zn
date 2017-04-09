3152ae54cc9c04507655a0de0e42ccc5e8ff11b8
siers <wimuan@gmail.com>
2017-04-09 13:11:46 +0300

    Remove highlights before doing sedsubst.

--- Zn/Commands/Sed.hs
+++ Zn/Commands/Sed.hs
@@ -14,12 +14,16 @@ import Zn.Commands.Logs
 import qualified Zn.Grammar as Gr
 import Zn.IRC
 
+unhigh :: String -> String
+unhigh = replaceRegex (toRegex "(\x02|\x03[0-9]{1,2}|\x1d)") ""
+
 highlight :: String -> String
 highlight text = "\x02\x1d\x03" ++ "04" ++ text ++ "\x0f"
 
 sed' :: String -> Seq.Seq [String] -> ((String, String), String) -> Maybe String
 sed' target history ((regex', subst), flags) =
-    join . find isJust . fmap perhapsSubst . limit . fmap (!! 2) $ history
+    join . find isJust . fmap perhapsSubst . fmap unhigh . limit . fmap (!! 2) $ history
+
     where
         limit = Seq.take 50 . Seq.filter (not . isJust . Gr.matches Gr.sed . pack)
         perhapsSubst msg = replacer regex (highlight subst) msg <$ (matchM regex msg :: Maybe String)
@@ -32,7 +36,9 @@ sed :: PrivEvent Text -> Bot ()
 sed pr = join $ fmap (sequence_ . fmap (reply pr . pack) . join) $
 
     Gr.ifParse Gr.sed (view cont pr) $
-        (sed' source <$> uses history tailor <*>) . return
+        \parsed -> do
+            latestHist <- uses history tailor
+            return $ sed' source latestHist parsed
 
     where
         tailor = logFrom source . logMap unpack
