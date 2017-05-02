9aab6cedfe05fa4e447f99004b021f462090bbd7
siers <wimuan@gmail.com>
2017-05-02 15:15:12 +0300

    Print !version with shorter SHA1 and handle absence of "public" remote.

--- Zn/Commands/VersionInternal.hs
+++ Zn/Commands/VersionInternal.hs
@@ -2,12 +2,15 @@ module Zn.Commands.VersionInternal (getVersion) where
 
 import Control.Monad.IO.Class
 import Data.List.Split
-import System.Process
+import qualified System.Process as P
 import Text.Printf
 
 cmd :: String -> IO String
-cmd code = liftIO $ readProcess cmd args ""
+cmd code = liftIO $ P.readProcess cmd args ""
     where cmd:args = splitOn " " code
+
+shell :: String -> IO String
+shell code = P.readCreateProcess (P.shell code) ""
 
 formatName :: String -> String
 formatName name = reverse . dropWhile (`elem` (".\n" :: String)) . reverse $
@@ -15,11 +18,17 @@ formatName name = reverse . dropWhile (`elem` (".\n" :: String)) . reverse $
     then take 80 name ++ "…"
     else name
 
+findURL :: String
+findURL = "\
+    \(git remote get-url public 2>/dev/null || \
+    \ git remote get-url origin 2>/dev/null) \
+    \ || echo URL_NOT_FOUND"
+
 getVersion :: IO String
 getVersion = fmap (filter (/= '\n')) $ printf statement <$> rev <*> str <*> date <*> origin
     where
         statement = "Running %s: «%s» of %s, %s"
         str = formatName <$> cmd "git show -s --format=%s"
-        rev = take 10 <$> cmd "git rev-parse HEAD"
+        rev = cmd "git rev-parse --short HEAD"
         date = cmd "git show -s --format=%ci"
-        origin = cmd "git remote get-url public"
+        origin = shell findURL
