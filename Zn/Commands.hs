cf8859c2006809b2473025ac3bd46a77373aa3c8
siers <wimuan@gmail.com>
2017-05-05 15:58:37 +0300

    Add «!sleep».

--- Zn/Commands.hs
+++ Zn/Commands.hs
@@ -9,7 +9,7 @@ import qualified Data.Map.Strict as M
 import Data.Maybe
 import Data.Monoid
 import qualified Data.Text as T
-import Data.Text as T (pack, Text)
+import Data.Text as T (unpack, pack, Text)
 import Zn.Bot
 import Zn.Command
 import Zn.Commands.Distribute
@@ -20,10 +20,10 @@ import qualified Zn.Grammar as Gr
 import Zn.IRC
 import Zn.Process
 
--- Args, Pure, Reply, Output(no input/args), Lift(IO), Monad(m a)
+-- Args, Pure, Reply, Output(no input/args), Lift(IO), Monad(noargs)
 
 command = (,)
-commandA name cmd = command name . cmd . view args
+commandA name cmd = command name $ cmd . view args
 commandR name cmd = command name $ \msg -> cmd msg >>= reply msg
 commandRA name cmd = commandR name $ cmd . view args
 commandO name = commandR name . const
@@ -39,11 +39,15 @@ commands :: M.Map Text (Command Text -> Bot ())
 commands = M.fromList
     [ commandPRA    "echo"         (T.intercalate " ")
     , commandPRA    "quote"        (\x -> "\"" <> T.intercalate "\" \"" x <> "\"")
+
     , commandPO     "version"      Zn.version
     , commandO      "uptime"       uptime
-    , commandRA     "botcast"    $ botcast . T.intercalate " "
-    , commandO      "replies"      Replies.list
+
     , commandM      "reload"       reload
+    , commandA      "sleep"      $ sleep . read . unpack . head
+
+    , commandO      "replies"      Replies.list
+    , commandRA     "botcast"    $ botcast . T.intercalate " "
     , commandLO     "iesauka"    $ pack <$> shell "./scripts/names-lv/bundle_wrapper.rb"
 
     -- leaks important data to chan, but might be useful for debugging sometimes
