6febb51a9f6e7e07d7eb63979c51103489c4bdb2
siers <wimuan@gmail.com>
2017-10-16 12:36:35 +0300

    Factor out Formatting from URL/Main.hs.

--- src/Zn/Commands/URL/Main.hs
+++ src/Zn/Commands/URL/Main.hs
@@ -1,5 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Zn.Commands.URL.Main where
+module Zn.Commands.URL.Main
+    ( url
+    , format
+    , parseTitle
+    ) where
 
 import Control.Arrow
 import Control.Lens
@@ -8,73 +12,20 @@ import Control.Monad.Catch
 import Control.Monad.IO.Class
 import Control.Retry
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.CaseInsensitive as CI
 import Data.List
-import Data.List.Split
-import qualified Data.Map.Strict as M
-import Data.Maybe
 import qualified Data.Text as T
-import Data.Text.Encoding as BSE
-import qualified Data.Text.Lazy as TL
-import Data.Text.Lazy.Encoding as BLE
 import Data.Text (Text)
 import Data.Tuple
-import Hledger.Utils.Regex (regexMatchesCI)
 import Network.HTTP.Client
 import Network.HTTP.Types
 import Prelude hiding (concat)
-import Text.HTML.TagSoup (Tag (..), parseTags, isTagOpen, isTagClose)
-import Text.Printf
 import Text.Regex.TDFA
+import Zn.Commands.URL.Format
 import Zn.IRC
 import Zn.TLS
 import Zn.Types
 
 userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36"
-
-tagName :: Tag String -> Maybe String
-tagName (TagOpen n _) = Just n
-tagName (TagClose n) = Just n
-tagName _ = Nothing
-
-tagNameRegCI :: Tag String -> String -> Bool
-tagNameRegCI t s = regexMatchesCI s $ fromMaybe "" (tagName t)
-
-textify :: [Tag String] -> String
-textify = filter (\c -> not $ c `elem` ("\r\n" :: String)) . concatMap text
-    where
-        text (TagText s) = s
-        text _           = ""
-
-parseTitle :: String -> Maybe String
-parseTitle = ifAny prepare . extract
-    where
-        ifAny f l = if l == [] then Nothing else Just (f l)
-        prepare = take 1000 . textify -- it gets cut smaller, still
-        dropCond t = not $ tagNameRegCI t "^title$" && isTagOpen t
-        takeCond t = not $ tagNameRegCI t "^title$" && isTagClose t
-        extract = takeWhile takeCond . dropWhile dropCond . parseTags
-
-format :: (BL.ByteString, ResponseHeaders) -> String
-format (body, rHeaders) =
-    concat .
-    (["¬ "] ++) .
-    intersperse " · " .
-    map (printf "%s") .
-    filter (not . null) $
-        [title] ++ (removeEncoding contentType \\ ["text/html"])
-
-    where
-        title = concat . map trim . maybeToList . parseTitle . bleUnpack $ body
-        trim = T.unpack . T.strip . T.pack
-
-        removeEncoding = fmap (\c -> splitOn ";" c !! 0)
-        contentType = fmap bseUnpack . maybeToList $
-            CI.mk "Content-Type" `M.lookup` M.fromList rHeaders
-
-        bleUnpack = TL.unpack . BLE.decodeUtf8With substInvalid
-        bseUnpack = T.unpack . BSE.decodeUtf8With substInvalid
-        substInvalid = return (const (Just ' '))
 
 request :: String -> IO (BL.ByteString, ResponseHeaders)
 request url = do
