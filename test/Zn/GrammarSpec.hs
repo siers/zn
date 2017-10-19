c8d19a10ca015b9ac3ad7ed142e1c75a35a994f6
siers <wimuan@gmail.com>
2017-10-19 14:42:51 +0300

    Tiny GrammarSpec refactor.

--- test/Zn/GrammarSpec.hs
+++ test/Zn/GrammarSpec.hs
@@ -3,20 +3,22 @@ module Zn.GrammarSpec (spec) where
 import Test.Hspec
 import Zn.Grammar
 
+parsesAs match expectation = match `shouldBe` Just expectation
+
 spec :: Spec
 spec = do
   describe "addressing" $ do
     it "addressable via !" $
-      matches (addressed "nick") "!cmd" `shouldBe` Just "cmd"
+      matches (addressed "nick") "!cmd" `parsesAs` "cmd"
 
     it "addressable via nick" $
-      matches (addressed "nick") "nick, cmd" `shouldBe` Just "cmd"
+      matches (addressed "nick") "nick, cmd" `parsesAs` "cmd"
 
     it "addressable with #()" $
-      matches (addressed "nick") "#(cmd)" `shouldBe` Just "cmd"
+      matches (addressed "nick") "#(cmd)" `parsesAs` "cmd"
 
     it "addressable with #() and escapable" $
-      matches (addressed "nick") "text #(\\)) more text" `shouldBe` Just ")"
+      matches (addressed "nick") "text #(\\)) more text" `parsesAs` ")"
 
     it "addressable only once" $
-      matches (addressed "nick") "text #(foo) more #(bar)" `shouldBe` Just "foo"
+      matches (addressed "nick") "text #(foo) more #(bar)" `parsesAs` "foo"
