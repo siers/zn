ae59e2d77ee0157d28a17989a1da1b1c27c1af46
siers <wimuan@gmail.com>
2017-10-19 16:57:43 +0300

    Add sed parser tests (1 failing).

--- test/Zn/GrammarSpec.hs
+++ test/Zn/GrammarSpec.hs
@@ -3,6 +3,7 @@ module Zn.GrammarSpec (spec) where
 import Test.Hspec
 import Zn.Grammar
 
+unparses match = match `shouldBe` Nothing
 parsesAs match expectation = match `shouldBe` Just expectation
 
 spec :: Spec
@@ -22,3 +23,19 @@ spec = do
 
     it "addressable only once" $
       matches (addressed "nick") "text #(foo) more #(bar)" `parsesAs` "foo"
+
+  describe "sed" $ do
+    it "doesn't parse" $
+      unparses $ matches sed ""
+
+    it "parses" $
+      matches sed "s/a/b/ggr" `parsesAs` [(("a", "b"), "ggr")]
+
+    it "parses without separator" $
+      matches sed "s/a/b" `parsesAs` [(("a", "b"), "")]
+
+    it "is concatenable" $
+      matches sed "s/a/b/; s/c/d" `parsesAs` [(("a", "b"), ""), (("c", "d"), "")]
+
+    it "is concatenable without end separators" $
+      matches sed "s/a/b; s/c/d" `parsesAs` [(("a", "b"), ""), (("c", "d"), "")]
