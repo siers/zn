d2ac473e32bed54fc0aa721490f6c4866c4fcede
siers <wimuan@gmail.com>
2018-02-19 09:18:21 +0200

    nix/: brush up the source, pin nixpkgs in default.nix.

--- nix/default.nix
+++ nix/default.nix
@@ -1,43 +1,48 @@
+# nix-prefetch-git https://github.com/nixos/nixpkgs-channels.git refs/heads/nixos-unstable > nixpkgs-version.json
 { pkgs ? import <nixpkgs> {} }:
 
-with pkgs;
+let
+  pinned = json:
+    let
+      version = pkgs.lib.importJSON (./nixpkgs- + json + ".json");
+      source = pkgs.fetchFromGitHub {
+        owner = "NixOS";
+        repo = "nixpkgs-channels";
+        inherit (version) rev sha256;
+      };
+    in import source {};
+
+  pkgs_17_09 = pinned "17.09";
+  pkgs_unstable = pinned "unstable";
+in
 
 let
-  hackagePackages' = haskellPackages.override {
-    overrides = self: super: let
-      in {
-        servant = haskell.lib.dontCheck (self.callHackage "servant" "0.10" {});
-        servant-client = haskell.lib.dontCheck (self.callHackage "servant-client" "0.10" {});
-      }; };
+  haskellPackages' = with pkgs_17_09; haskellPackages.override {
+    overrides = self: super: with haskell.lib;
+      {
+        servant = dontCheck (self.callHackage "servant" "0.10" {});
+        servant-client = dontCheck (self.callHackage "servant-client" "0.10" {});
+        irc-client = dontCheck (super.callHackage "irc-client" "1.0.0.1" {});
+      };
+  };
 
-  irc-client = haskellPackages.callPackage (import ./irc-client.nix) {};
-  telegram-api = hackagePackages'.telegram-api;
-
-  zn = { mkDerivation, aeson, array, async, base, base64-bytestring, binary
-    , bytestring, case-insensitive, conduit, conduit-combinators
-    , conduit-extra, connection, containers, cryptonite, data-default, either, exceptions
-    , extra, filepath, http-client, http-client-tls, http-types, ini, irc-client, irc-ctcp
-    , irc-conduit, lens, megaparsec, mtl, network, network-simple, process, regex-tdfa
-    , regex-tdfa-text, retry, safe, split, stdenv, stm, stm-chans
-    , streaming-commons, strict, strptime, tagged, tagsoup, telegram-api
-    , template-haskell, text, text-format, text-icu, text-regex-replace, time, tls, transformers
-    , uglymemo, unix, unix-time, unordered-containers, x509-system, xml-conduit, hspec
-    , pkgs
-    }:
+  zn = { aeson, array, async, base, base64-bytestring, binary, bytestring
+    , case-insensitive, conduit, conduit-combinators, conduit-extra
+    , connection, containers, cryptonite, data-default, either, exceptions
+    , extra, filepath, http-client, http-client-tls, http-types, ini
+    , irc-client, irc-ctcp, irc-conduit, lens, megaparsec, mtl, network
+    , network-simple, process, regex-tdfa, regex-tdfa-text, retry, safe, split
+    , stdenv, stm, stm-chans, streaming-commons, strict, strptime, tagged
+    , tagsoup, telegram-api, template-haskell, text, text-format, text-icu
+    , text-regex-replace, time, tls, transformers, uglymemo, unix, unix-time
+    , unordered-containers, x509-system, xml-conduit, hspec, hpack
+    , mkDerivation
+    }@args:
 
     let
-      deps = [
-        aeson array async base base64-bytestring binary
-        bytestring case-insensitive conduit conduit-combinators
-        conduit-extra connection containers cryptonite data-default either exceptions
-        extra filepath http-client http-client-tls http-types ini irc-client irc-ctcp
-        irc-conduit lens megaparsec mtl network network-simple process regex-tdfa
-        regex-tdfa-text retry safe split stdenv stm stm-chans
-        streaming-commons strict strptime tagged tagsoup telegram-api
-        template-haskell text text-format text-icu text-regex-replace time tls transformers
-        uglymemo unix unix-time unordered-containers x509-system xml-conduit hspec
-        git coreutils
-      ];
+      hsDeps = builtins.attrValues (removeAttrs args [ "mkDerivation" ]);
+      cliDeps = with pkgs_17_09; [ git coreutils ];
+      deps = hsDeps ++ cliDeps;
     in
       mkDerivation {
         pname = "zn";
@@ -48,12 +53,18 @@ let
         enableSharedExecutables = false;
         enableSharedLibraries   = false;
         executableHaskellDepends = deps;
-        testHaskellDepends = deps;
+        testHaskellDepends = hsDeps;
         description = "IRC bot";
         license = stdenv.lib.licenses.free;
       };
 
 in
-  haskell.lib.dontCheck
-    (haskell.lib.justStaticExecutables
-        (haskellPackages.callPackage zn { inherit irc-client telegram-api; }))
+  with pkgs_17_09;
+  with haskell.lib;
+  with haskellPackages;
+
+  dontCheck
+    (justStaticExecutables
+      (callPackage zn {
+        inherit (haskellPackages') irc-client telegram-api;
+      }))
