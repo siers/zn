35fc36017f671edd0b598861dde47ad36e09abe3
siers <wimuan@gmail.com>
2018-02-25 00:23:35 +0200

    Sort the botcasted !uptimes.

--- src/Zn/Commands/Distribute.hs
+++ src/Zn/Commands/Distribute.hs
@@ -11,34 +11,50 @@ import Control.Monad.IO.Class
 import Data.Foldable
 import qualified Data.List as L
 import qualified Data.Map.Strict as M
+import Data.Maybe
+import Data.Monoid
+import Data.Text (unpack)
 import qualified Data.Text as T
-import Data.Text.Format
+import qualified Data.Text.Format as TF
 import qualified Data.Text.Lazy as TL
 import Data.Text (Text)
 import Data.Time.Clock.POSIX
+import Text.Regex.TDFA
 import Network.IRC.Client
 import Zn.Bot
 import Zn.Commands.Logs
 import Zn.IRC
 import Zn.Types
 
-amass :: History Text -> [Text]
-amass = map (TL.toStrict . concat) . filter (not . null . snd) . M.toList
+type DistMsg = (Text, [Text])
+
+format :: [DistMsg] -> Text
+format = T.intercalate ", " . map (TL.toStrict . TF.format "{}[{}]" . second joinCmds)
+
+mbyUptimeSort :: Text -> [DistMsg] -> [DistMsg]
+mbyUptimeSort payload =
+    if isNothing $ ((unpack payload) =~~ ("^!?uptime\\s*$" :: String) :: Maybe String)
+    then id
+    else reverse . L.sortBy (curry $ strNumOrd . (times *** times))
     where
-        join = joinCmds . fmap (view text) . toList
-        concat = format "{}[{}]" . second join
+        strNumOrd (a, b) = compare (length a) (length b) <> compare a b
+        times :: DistMsg -> [Integer]
+        times = map (read . unpack) . T.words . T.filter (`elem` (" 1234567890" :: [Char])) . head . snd
+
+amass :: History Text -> [DistMsg]
+amass = map (second $ fmap (view text) . toList) . filter (not . null . snd) . M.toList
 
 transfer :: Text -> Text -> StatefulBot ()
 transfer payload nick = send . Privmsg nick . Right $ payload
 
-distribute :: Text -> [Text] -> Bot Text
+distribute :: Text -> [Text] -> Bot (History Text)
 distribute payload users = do
     mapM_ (Bot . transfer payload) $ users
 
     start <- liftIO getPOSIXTime
     sleep $ 1 + length users * 1
 
-    T.intercalate ", " . amass . logsFrom users . logTail start <$> use history
+    logsFrom users . logTail start <$> use history
 
 botnicks :: Bot [Text]
 botnicks = do
@@ -48,4 +64,4 @@ botnicks = do
 botcast :: Text -> Bot Text
 botcast payload =
     lock "botcast" $ do
-        distribute payload =<< botnicks
+        format . mbyUptimeSort payload . amass <$> (distribute payload =<< botnicks)
