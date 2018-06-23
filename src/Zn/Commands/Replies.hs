857ec3ed900eb753b78dc76cd3e64054de153e5b
siers <wimuan@gmail.com>
2018-06-23 16:50:14 +0300

    Put anonymize into Replies.hs

--- src/Zn/Commands/Replies.hs
+++ src/Zn/Commands/Replies.hs
@@ -7,14 +7,29 @@ import Control.Monad.IO.Class
 import Database.Groundhog as G
 import Data.Maybe
 import Data.Monoid
-import Data.Text (Text, pack)
+import Data.Text (Text, pack, unpack)
 import Network.IRC.Client hiding (reply)
 import Safe
 import System.Random
 import Zn.IRC
 import Zn.Persist
-import Zn.Telegram (anonymize)
 import Zn.Types
+
+-- anonymize
+import Crypto.Hash
+import qualified Data.ByteString.Char8 as BS
+import Numeric
+
+-- A rather injective function into meaningless names.
+anonymize :: String -> Int -> Text -> Text
+anonymize alphabet len seed =
+    pack . take len $
+        (alphabet !!) . fromIntegral . (`mod` l) <$>
+            iterate (`div` l) rnd
+    where
+        l = fromIntegral $ length alphabet :: Integer
+        h = hash :: BS.ByteString -> Digest Keccak_512
+        rnd = fst . head . readHex . show . h . BS.pack . unpack $ seed :: Integer
 
 aliasAlphabet = ['A'..'Z'] ++ ['a'..'z'] ++ ['0'..'9']
 
